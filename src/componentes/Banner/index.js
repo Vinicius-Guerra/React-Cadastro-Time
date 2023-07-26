13014a0418b14de905b838bdf3788a93a98e74e6
Vinicius Guerra <vini.fms@hotmail.com>
2023-07-26 17:07:32 -0300

    Projeto em andamento

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import './banner.css';
+
+function Banner() {
+    //JSX
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner principal da pagina do organo."/>
+        </header>
+    )
+}
+
+export default Banner;
